feeb881a87a8e76fdbe2e8b04bc325045c4b83c2
Cosme Teixeira Da Silva <cosme.teixeira@gmail.com>
2026-06-03 14:24:33 -0300

    alteraçoes

--- app/page.js
+++ app/page.js
@@ -8,12 +8,13 @@ export default function Home() {
     <div className="min-h-screen flex items-center justify-center bg-zinc-100 dark:bg-zinc-950 px-4">
       <main className="w-full max-w-4xl bg-slate-200 dark:bg-zinc-900 rounded-2xl shadow-xl p-8 md:p-16">
         <div className="flex flex-col items-center text-center gap-8">
-          <h1 className="text-4xl md:text-6xl font-extrabold text-orange-500 dark:text-zinc-100">
+          <h1 className="text-2xl md:text-2xl font-extrabold text-orange-500 dark:text-zinc-100">
             Cosme Teixeira Silva
           </h1>
 
           <p className="text-lg md:text-xl text-zinc-600 dark:text-zinc-400 max-w-2xl">
-            Aula prática de geração de documentos PDF utilizando React,
+            Aula prática de geração de documentos PDF utilizando React, CSS, TailWind e Next.js.
+            Neste projeto, criamos um formulário para coletar informações do usuário e gerar um currículo personalizado em PDF,
             explorando conceitos modernos de componentes, estilização e
             exportação de arquivos.
           </p>
